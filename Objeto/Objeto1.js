f4a39442747ac08cc9cad88e086df3e431dddd29
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2025-06-23 16:20:34 +0000

    oi

---
+++ Objeto/Objeto1.js
@@ -0,0 +1,8 @@
+const lugar = {
+    País: "Alemanha",
+    Continente: "Europa",
+    mudarEpoca: function() {
+        console.log("1945!!!, seja amigo do mano adolfinho");
+    }
+};
+lugar.mudarEpoca()
---
+++ Objeto/Objeto2.js
@@ -0,0 +1,6 @@
+const jogos = {
+      gigas: "180",
+      preço: 100,
+      nomeDoJogo: ["Fortnite", "Cod", "RocketLeague"]
+}
+console.log(jogos)
